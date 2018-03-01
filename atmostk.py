e14a447522662255426e9d5b6b0409f45c328d5c
Victor Nicolet <victor.nicolet@polytechnique.edu>
2018-02-28 22:10:49 -0500

    Adding encodings: commander and sequential counter.

--- atmostk.py
+++ atmostk.py
@@ -31,13 +31,16 @@ def nthbit(c,n):
     return c & (1 << (n-1)) != 0
 
 
+def neg_all(l):
+    return list(map(lambda x: -x, l))
+
+
 def atmostk_bin(l, k):
-    return list(map(lambda x: list(map(lambda i: -i, x)),
-                    list(itertools.combinations(l, k))))
+    return list(itertools.combinations(neg_all(l), k + 2))
 
 
 def atleastk_bin(l, k):
-    return list(map(lambda x: list(x), list(itertools.combinations(l, len(l) - k))))
+    return list(itertools.combinations(l, len(l)-k))
 
 
 def binary_encoding(k,n):
@@ -73,8 +76,6 @@ def binary_encoding(k,n):
             for j in rangeincl(1, log2n):
                 clauses.append([-T(g, i), Phi(i, g, j)])
 
-    # Add 'at least one' constraint
-    clauses.extend(atleastk_bin(rangeincl(1, n), max(1, k-3)))
 
     return {'name': ("binary_k%d_n%d" % (k, n)),
             'nvars': n + log2n * k + n * k,
@@ -82,29 +83,72 @@ def binary_encoding(k,n):
             'clauses': clauses}
 
 
-
 def commander_encoding(k, n, s):
-    if s < k:
+    nsets = ceil(n/s)
+
+    if nsets < k:
         print("For commander encoding, size should be greater than constraint.")
-        return
+        return -1
 
-    nsets = ((n - 1) / s) + 1
-
-    def c(i,j):
-        return n + i * nsets + j
+    def C(i, j):
+        return n + i * k + j
 
     clauses = []
+    # For each group
+    for i in range(0, nsets):
+        gis = list(rangeincl(1 + i * s, min(1 + (i + 1) * s, n)))
+        cis = list(map(lambda j: -C(i, j), list(rangeincl(1, k))))
+        gis.extend(cis)
+
+        clauses.extend(atmostk_bin(gis, k))
+        clauses.extend(atleastk_bin(gis, k))
+        # Remove symmetrical solutions by ordering commander variables
+        for j in rangeincl(1, k-1):
+            clauses.append([-C(i, j), C(i, j+1)])
+
+    # Encoding cardinality on the commander variables
+    # Using simply the binomial encoding since tracking new variables
+    # here would require additional work
+    clauses.extend(atmostk_bin(list(map(lambda ij: C(ij[0], ij[1]),
+                                        list(itertools.product(range(0, nsets), rangeincl(1, k))))), k))
 
 
+    return {'name': ("commander_k%d_n%d_s%d" % (k, n, s)),
+            'nvars': n + nsets * k,
+            'nclauses': len(clauses),
+            'clauses': clauses}
 
-    return { 'name': ("commander_k%d_n%d_s%d" % (k, n, s)),
-             'nvars': n,
-             'nclauses': len(clauses),
-             'clauses': clauses}
+
+def seqcount_encoding(k, n):
+    clauses = []
+    def R(i,j):
+        return n + i * k + j
+    #(1)
+    for i in rangeincl(1, n):
+        clauses.append([-i, R(i, 1)])
+    # (2)
+    for j in rangeincl(2, k):
+        clauses.append([-R(1, j)])
+    # (3)
+    for i in rangeincl(2, n-1):
+        for j in rangeincl(1, k):
+            clauses.append([-R(i-1, j), R(i, j)])
+    # (4)
+    for i in rangeincl(2, n-1):
+        for j in rangeincl(2, k):
+            clauses.append([-i, -R(i-1, j-1), R(i, j)])
+    # (5)
+    for i in rangeincl(1,n):
+        clauses.append([-i, R(i-1, k)])
+
+    return {'name': ("seqcount_k%d_n%d" % (k, n)),
+            'nvars': n + k * (n-1),
+            'nclauses': len(clauses),
+            'clauses': clauses}
 
 def main():
     if len(sys.argv) < 4:
-        print("Usage: python atmostk.py NVARS K [ENCODING=1,2]")
+        print("Usage: python atmostk.py NVARS K [ENCODING=1,2] [SIZE if ENCODING=2]")
         return
 
     n = int(sys.argv[1])
@@ -113,7 +157,7 @@ def main():
     encoding = {}
 
     if en == 1:
-        encoding = binary_encoding(k,n)
+        encoding = binary_encoding(k, n)
     elif en == 2:
         if len(sys.argv) < 5:
             print("For encoding 2 (commander encoding) you have to provide a size (last argument)")
@@ -121,11 +165,20 @@ def main():
 
         s = int(sys.argv[4])
         encoding = commander_encoding(k, n, s)
+
+    elif en == 3:
+        encoding = seqcount_encoding(k, n)
     else:
         print("Unkown encoding.")
         return
 
+    if encoding == -1:
+        return
+
+    print("Encoding done!\n")
+
     print_encoding(encoding)
+
     minisat(encoding)
 
 
