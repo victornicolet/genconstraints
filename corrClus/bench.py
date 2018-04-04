01dfeefa0c2afe51043beef094ad743c12f9e8e4
Victor Nicolet <victorn@cs.toronto.edu>
2018-04-04 15:58:28 -0400

    Update results

--- corrClus/bench.py
+++ corrClus/bench.py
@@ -2,7 +2,8 @@ import correlation_clustering as ccs
 import glob
 
 BENCHMARKS = './bench/*_benchmark'
-CSV_HEADER = "id,numpoints,numweights,numvars,numclauses,numclusters.timeout,time(solve),time(encode),time(cluster),memory"
+CSV_HEADER = "id,#points,#weights,#clauses,#clusters,timeout,solving time (s)," \
+             "encoding time (s),cluster construction time (s),memory(MB)"
 
 def bench():
     all_benchmarks = glob.glob(BENCHMARKS)
@@ -10,7 +11,7 @@ def bench():
     for benchmark in all_benchmarks:
         ccs_instance = ccs.solve(benchmark, timeout=500)
         print(ccs_instance)
-        results.append([ccs_instance.file, len(ccs_instance.weights), len(ccs_instance.data_points),
+        results.append([ccs_instance.file, len(ccs_instance.data_points), len(ccs_instance.weights),
                         len(ccs_instance.clauses), len(ccs_instance.clusters), ccs_instance.timed_out, ccs_instance.solve_time,
                         ccs_instance.encode_time, ccs_instance.cluster_time, ccs_instance.solve_memory])
 
